4767406fe9f930061b3222aa86a7f6d25268afad
Artur Quirino <artur.pquirino@gmail.com>
2020-09-26 10:50:55 -0300

    change the layout of the header card

--- refund-app/src/pages/currentRefund/headerCurrentRefund/HeaderCurrentRefund.jsx
+++ refund-app/src/pages/currentRefund/headerCurrentRefund/HeaderCurrentRefund.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from '../../../components/card/Card';
 import './HeaderCurrentRefund.css';
 
 class HeaderCurrentRefund extends Component {
@@ -9,17 +10,36 @@ class HeaderCurrentRefund extends Component {
   render() {
     const { headerData } = this.props;
     return (
-      <div className="header-current-refund">
-        <h1>{headerData.title}</h1>
-        <div>
-          <span>Nome</span>
+      <Card className="header-current-refund">
+        <h1 className="header-current-refund__header">{headerData.title}</h1>
+        <div className="header-current-refund__properties">
+          <span className="header-current-refund__properties__label">Nome</span>
           <span>{headerData.name}</span>
-          <span>E-mail</span>
+          <span className="header-current-refund__properties__label">
+            E-mail
+          </span>
           <span>{headerData.email}</span>
-          <span>Justificativa</span>
+          <span className="header-current-refund__properties__label">
+            Justificativa
+          </span>
           <span>{headerData.justification}</span>
         </div>
-      </div>
+        <div className="header-current-refund__analist">
+          <div className="header-current-refund__analist-cost-center__analist">
+            <label htmlFor="analist">Atribuir analista</label>
+            <select name="analist" name="analist">
+              <option>A</option>
+              <option>b</option>
+              <option>c</option>
+              <option>d</option>
+            </select>
+          </div>
+          <div className="header-current-refund__analist-cost-center__cost-center">
+            <span>Centro de Custo</span>
+            <span>100% - Approval CC Test</span>
+          </div>
+        </div>
+      </Card>
     );
   }
 }
